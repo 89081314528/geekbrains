e54876c37ea6bbae78a4a8845a5fc9c7b4e36d42
julia <89081314528@mail.ru>
2021-06-02 19:58:28 +0300

    lesson7

---
+++ src/main/java/ru/geekbrains/lesson7/Lesson7.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson7;
+
+public class Lesson7 {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Barsik", 5);
+        Plate plate = new Plate(100);
+        plate.info();
+        cat.eat(plate);
+        plate.info();
+    }
+}
+
+
