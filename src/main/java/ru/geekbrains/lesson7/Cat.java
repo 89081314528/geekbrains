683a801b35a55ea7d37b3a1992af13d157283e42
julia <89081314528@mail.ru>
2021-06-04 00:01:37 +0300

    lesson7 1

--- src/main/java/ru/geekbrains/lesson7/Cat.java
+++ src/main/java/ru/geekbrains/lesson7/Cat.java
@@ -1,14 +1,36 @@
 package ru.geekbrains.lesson7;
 
+import java.sql.SQLOutput;
+
 public class Cat {
-    private String name;
-    private int appetite;
+    private final String name;
+    private final int appetite;
+    private boolean full = false;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isFull() {
+        return full;
+    }
+    public int getAppetite() {
+        return appetite;
+    }
+
     public Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
     }
-    public void eat(Plate p) {
-        p.decreaseFood(appetite);
-    }
 
+        public void eat(Plate p) {
+            if (appetite > p.getFood()) {
+                System.out.println("Кот " + name + " не может есть из тарелки");
+            } else {
+                p.decreaseFood(appetite);
+                System.out.println("Кот " + name + " поел из тарелки");
+                full = true;
+            }
+        }
 }
--- src/main/java/ru/geekbrains/lesson7/Plate.java
+++ src/main/java/ru/geekbrains/lesson7/Plate.java
@@ -2,11 +2,16 @@ package ru.geekbrains.lesson7;
 
 public class Plate {
     private int food;
+
+    public int getFood() {
+        return food;
+    }
+
     public Plate(int food) {
         this.food = food;
     }
     public void decreaseFood(int n) {
-        food -= n;
+            food = food - n;
     }
     public void info() {
         System.out.println("plate: " + food);
