1a761c5902ea0391a72fcbe38f4d84b16e57274a
julia <89081314528@mail.ru>
2021-08-27 19:50:21 +0300

    27/08

---
+++ src/main/java/ru/geekbrains/Massive.java
@@ -0,0 +1,13 @@
+package ru.geekbrains;
+
+public class Massive <T>{
+    T[] massive;
+
+    public T[] getMassive() {
+        return massive;
+    }
+
+    public Massive(T[] massive) {
+        this.massive = massive;
+    }
+}
